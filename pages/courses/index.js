4100bbc09c56c52b807cc663599a326630f9a3f9
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-16 15:45:01 +0500

    :see_no_evil: updated the .gitignore

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const getStaticProps = async () => {
+
+}
+
 const Courses = () => {
     return ( 
         <h2>These are all the courses</h2>
